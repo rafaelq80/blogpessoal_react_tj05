29503df4d290546a105ea574db8b0b194797eef8
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-08-27 02:02:32 -0300

    Código Comentado

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
 import Login from "./pages/login/Login"
+import { AuthProvider } from "./contexts/AuthContext"
 
 function App() {
   // Código TypeScript
@@ -11,18 +12,26 @@ function App() {
   return (
     // Código TSX => HTML + CSS
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      {/* 
+          Envolvemos todos os Componentes inseridos no Componente App, 
+          com o Componente AuthProvider. 
+          Desta forma, todos os Componentes dentro dele, terão acesso 
+          aos Estados e Funções guardados na context.
+      */}
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,56 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import AuthContext from "../../contexts/AuthContext";
 
 function Navbar() {
+
+   /**
+     * Cria a constante navigate, que receberá o Hook useNavigate().
+     * Através da constante navigate, o usuário será redirecionado 
+     * para outras rotas da aplicação, conforme a necessidade.
+     */
+   const navigate = useNavigate()
+
+   /**
+    * Criamos uma desestruturação para receber a função handleLogout(), 
+    * disponível na Context AuthContext, através do Hook useContext(). 
+    */
+   const { handleLogout } = useContext(AuthContext)
+
+   /**
+    * Criamos a função logout(), que será responsável por efetuar 
+    * o processo de logout do usuário. A função logout() executa a 
+    * função handleLogout() da Context AuthContext, responsável por 
+    * reiniciar os dados do usuário no contexto da aplicação, ou seja, 
+    * retornando para o Estado inicial. 
+    * 
+    * Na sequência, será exibido um alerta para informar o usuário que 
+    * o logout foi efetuado com sucesso e redireciona o usuário para a 
+    * Página de Login, através da constante navigate. 
+    */
+   function logout() {
+       handleLogout()
+       alert('O usuário foi desconectado com sucesso!')
+       navigate('/')
+   }
+   
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
-            
+
                 <div className="container flex justify-between text-lg">
-                
-                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens
                         Temas
                         Cadastrar tema
                         Perfil
-                        Sair
+                        <Link to='' onClick={logout} className="hover:underline">
+                            Sair
+                        </Link>
                     </div>
                 </div>
             </div>
